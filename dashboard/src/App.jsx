326534383009b0ffb57b76d0545878ada9abba53
Nimira43 <n.rabone72@aol.com>
2024-12-23 03:13:25 +0000

    Updated Home page.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,8 +2,7 @@ function App() {
   return (
     <div>
       <h1 className='logo text-3xl'>CyberMarket</h1>
-      <h4 className='title'>Online Retailers</h4>
-      <p>&copy; NimiraTech 2024</p>
+      <h2>Home Page</h2>
     </div>
   )
 }
