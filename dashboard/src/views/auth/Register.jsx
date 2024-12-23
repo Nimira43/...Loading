7d9913a07362e01749a7f113efe55eb1095ad387
Nimira43 <n.rabone72@aol.com>
2024-12-23 04:45:42 +0000

    Setting up welcoming messages to the Register form

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className='min-w-screen min-h-screen bg-[#fffaf0] flex justify-center items-center'>
+      <div className='w-[350px] text-[#fffaf0] p-2'>
+        <div className='bg-[#ff4500] p-4 rounded-md'>
+          <h2 className='text-xl mb-5 font-bold'>The Cyber Market</h2>
+          <p className='text-sm mb-3 font-medium'>Please signup to start your account</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
