7e55913a1c91f86f6f403bf817b74236ce71a90a
Nimira43 <n.rabone72@aol.com>
2024-12-23 02:44:54 +0000

    Tesing Tailwind and setting up some base Vanilla styles.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className='logo'>CyberMarket</h1>
+      <h1 className='logo text-3xl'>CyberMarket</h1>
       <h4 className='title'>Online Retailers</h4>
       <p>&copy; NimiraTech 2024</p>
     </div>
