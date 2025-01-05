7e636de95250b2732b762656c832d1b4ab744502
Nimira43 <n.rabone72@aol.com>
2025-01-05 08:25:55 +0000

    Register button styled and link to Login page done.

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
     <div className='min-w-screen min-h-screen bg-[#fffaf0] flex justify-center items-center'>
       <div className='w-[350px] text-[#fffaf0] p-2'>
         <div className='bg-[#ff4500] p-4 rounded-md'>
-          <h2 className='text-3xl mb-5 font-bold logo'>The Cyber Market</h2>
-          <p className='text-sm mb-3 font-medium'>Please register to start your account</p>
+          <h2 className='text-3xl mb-5 font-bold logo text-center'>The Cyber Market</h2>
+          <p className='text-sm mb-3 font-medium text-center'>Please register to start your account</p>
           <form>
             <div className='flex flex-col w-full gap-1 mb-3'>
               <label htmlFor='name'>Name</label>
@@ -22,8 +23,12 @@ const Register = () => {
             </div>
 
             <div className='flex items-center w-full gap-3 mb-3'>
-            <input className='h-4 w-4 text-gray-950 overflow-hidden bg-white rounded border-white focus:ring-black' type='checkbox' name='checkbox' id='checkbox'  />
-            <label htmlFor='checkbox' className='text-sm font-medium'>I agree to the terms and conditions</label>
+              <input className='h-4 w-4 text-[#333] overflow-hidden bg-white rounded border-white focus:ring-black' type='checkbox' name='checkbox' id='checkbox' />
+              <label htmlFor='checkbox' className='text-sm font-medium'>I agree to the terms and conditions</label>
+            </div>
+            <button className='bg-[#fffaf0] text-[#ff4500] w-full hover:text-[#333] uppercase rounded-md px-7 py-2 mb-3' >Sign Up</button>
+            <div className='flex items-center mb-3 gap-3 justify-center text-sm'>
+              <p>Already have an account? <Link className='font-semibold' to='/login'>Sign In</Link></p>
             </div>
           </form>
         </div>
