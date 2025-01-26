6dd4f5c3f74fdcb6ac4005eca686efbe04872bee
Nimira43 <n.rabone72@aol.com>
2025-01-26 03:30:56 +0000

    Issues with configuring Tailwind. Tailwind 4 installation is different. Manually set up Tailwind 3

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,11 @@
 function App() {
   return (
-    <div>
-      <h1>Cyber Markey</h1>
+    <div className='p-10 bg-[#111]'>
+      <h1 className='text-3xl logo text-[#ff4500] font-medium'>Cyber Market</h1>
+      <hr />
+      <h2 className='uppercase text-[#fffaf0]' >Dashboard</h2>      
     </div>
   )
 }
 
 export default App
-
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,5 +1,3 @@
-// Vite - main.jsx
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -11,27 +9,10 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-// Create React App - old way - has the root js logic for project
-
-// import React, {lazy} from 'react'
-// import ReactDOM from 'react-dom/client'
-// import './index.css'
-// import { BrowserRouter } from 'react-router-dom'
-// const App = lazy(() => import('./App'))
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// )
-
-// New Code
-
-// import { StrictMode, lazy, Suspense } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import './index.css'
+// import { StrictMode, lazy, Suspense } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
 
 // const App = lazy(() => import('./App.jsx'));
 
@@ -43,4 +24,4 @@ createRoot(document.getElementById('root')).render(
 //       </Suspense>
 //     </BrowserRouter>
 //   </StrictMode>,
-// )
+// );
