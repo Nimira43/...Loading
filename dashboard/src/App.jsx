a8b9c8ed2cfa3520c279ed00e746196a5cc1e7e6
Nimira43 <n.rabone72@aol.com>
2025-01-26 03:59:55 +0000

    Going to install dependencies next.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -9,3 +9,16 @@ function App() {
 }
 
 export default App
+
+
+// import { useState } from 'react'
+// import Router from './router/Router'
+// import publicRoutes from './router/routes/publicRoutes'
+
+// function App() {
+//   const [allRoutes, setAllRoutes] = useState([...publicRoutes])
+  
+//   console.log(allRoutes)
+//   return <Router allRoutes={allRoutes} />
+// }
+// export default App
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -9,6 +9,9 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
+// Use this -V-
+
+
 // import { StrictMode, lazy, Suspense } from 'react';
 // import { createRoot } from 'react-dom/client';
 // import { BrowserRouter } from 'react-router-dom';
@@ -25,3 +28,22 @@ createRoot(document.getElementById('root')).render(
 //     </BrowserRouter>
 //   </StrictMode>,
 // );
+
+
+
+
+// Old
+
+
+// import React, {lazy} from 'react'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom'
+// const App = lazy(() => import('./App'))
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// )
