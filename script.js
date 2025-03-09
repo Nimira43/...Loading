e8a405b7e5114fa1f036d2ce2d9be33b32982c8e
Nimira43 <n.rabone72@aol.com>
2025-03-09 08:55:19 +0000

    script.js and styles.css created, favicon brought in, files connected. Current toggle menu icon script commented out for the moment.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const menuIcon = document.querySelector('.menu-icon')
-const container = document.querySelector('.container')
+// const menuIcon = document.querySelector('.menu-icon')
+// const container = document.querySelector('.container')
 
-menuIcon.addEventListener('click', () => container.classList.toggle('change'))
+// menuIcon.addEventListener('click', () => container.classList.toggle('change'))
+
+console.log('Connected!!!')
